94577ab8e30f2bf992f4b7feab133fda09cb050b
Alexander Osipov <shuraosipov@gmail.com>
2023-02-27 12:21:23 -0500

    linted and typed

--- decorators/decorators.py
+++ decorators/decorators.py
@@ -3,8 +3,10 @@ import string
 import time
 import functools
 
+
 def timer(func):
     """Print the run time of the decorated function"""
+
     @functools.wraps(func)
     def wrapper(*args, **kwargs):
         start_time = time.perf_counter()
@@ -13,13 +15,16 @@ def timer(func):
         run_time = end_time - start_time
         print(f"Function {func.__name__!r} finished in {run_time:.4f} secs")
         return value
+
     return wrapper
 
+
 @timer
-def generate_string(length=8) -> None:
+def generate_string(length=8) -> str:
     alphabet = string.ascii_letters + string.digits
-    password = ''.join(secrets.choice(alphabet) for i in range(length))
+    password = "".join(secrets.choice(alphabet) for i in range(length))
     return password
+
 
 @timer
 def waste_some_time(num_times):
@@ -27,11 +32,6 @@ def waste_some_time(num_times):
         sum([i**2 for i in range(10000)])
 
 
-
 print(generate_string(length=12))
 
 waste_some_time(999)
-
-
-
-
